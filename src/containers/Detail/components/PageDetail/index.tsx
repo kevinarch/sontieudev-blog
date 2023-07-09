866900c4e64c9191850d2a7dd85689b2d1169316
Son Tieu <ttson.hcmus@gmail.com>
2023-07-09 10:01:09 +0700

    feat(react-tron-x): add direct imports instead of dynamic loading

--- src/containers/Detail/components/PageDetail/index.tsx
+++ src/containers/Detail/components/PageDetail/index.tsx
@@ -4,32 +4,26 @@ import React from "react"
 import dynamic from "next/dynamic"
 import Link from "next/link"
 import Image from "next/image"
-const Code = dynamic(() =>
-  import("react-notion-x/build/third-party/code").then(async (m) => {
-    await Promise.all([
-      import('prismjs/components/prism-bash.js'),
-      import('prismjs/components/prism-c.js'),
-      import('prismjs/components/prism-cpp.js'),
-      import('prismjs/components/prism-dart.js'),
-      import('prismjs/components/prism-swift.js'),
-      import('prismjs/components/prism-objectivec.js'),
-      import('prismjs/components/prism-kotlin.js'),
-      import('prismjs/components/prism-typescript.js'),
-      import('prismjs/components/prism-sql.js'),
-      import('prismjs/components/prism-docker.js'),
-      import('prismjs/components/prism-java.js'),
-      import('prismjs/components/prism-git.js'),
-      import('prismjs/components/prism-go.js'),
-      import('prismjs/components/prism-makefile.js'),
-      import('prismjs/components/prism-markdown.js'),
-      import('prismjs/components/prism-python.js'),
-      import('prismjs/components/prism-rust.js'),
-      import('prismjs/components/prism-yaml.js')
-    ])
+import Code from "react-notion-x/build/third-party/code"
 
-    return m.Code;
-  })
-)
+import "prismjs/components/prism-bash.js"
+import "prismjs/components/prism-c.js"
+import "prismjs/components/prism-dart.js"
+import "prismjs/components/prism-swift.js"
+import "prismjs/components/prism-objectivec.js"
+import "prismjs/components/prism-kotlin.js"
+import "prismjs/components/prism-typescript.js"
+import "prismjs/components/prism-sql.js"
+import "prismjs/components/prism-docker.js"
+import "prismjs/components/prism-java.js"
+import "prismjs/components/prism-git.js"
+import "prismjs/components/prism-go.js"
+import "prismjs/components/prism-makefile.js"
+import "prismjs/components/prism-markdown.js"
+import "prismjs/components/prism-python.js"
+import "prismjs/components/prism-rust.js"
+import "prismjs/components/prism-yaml.js"
+
 const Collection = dynamic(() =>
   import("react-notion-x/build/third-party/collection").then(
     (m) => m.Collection
