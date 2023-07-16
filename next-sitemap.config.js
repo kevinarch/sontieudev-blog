de73c9c5d441b850a10b1a7ffb9bdeb4164b80dc
Son Tieu <ttson.hcmus@gmail.com>
2023-07-16 18:41:58 +0700

    feat: update sitemap to ignore post with "Share" type

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,19 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
+  transform: async (config, path) => {
+    // custom function to ignore the path
+    if (path.startsWith("[skip]")) {
+      return null
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 }
