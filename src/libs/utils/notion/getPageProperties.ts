f0d47e569eb17b2077f19b98e44da95fea66befe
Kevin Arch <admin@sontieu.dev>
2026-02-16 11:41:01 +0700

    update getPageProperties

--- src/libs/utils/notion/getPageProperties.ts
+++ src/libs/utils/notion/getPageProperties.ts
@@ -9,7 +9,8 @@ async function getPageProperties(
   schema: CollectionPropertySchemaMap
 ) {
   const api = new NotionAPI()
-  const rawProperties = Object.entries(block?.[id]?.value?.value?.properties || [])
+  const blockId = block?.[id]?.value as any;
+  const rawProperties = Object.entries(blockId.value?.properties || [])
   const excludeProperties = ["date", "select", "multi_select", "person", "file"]
   const properties: any = {}
   for (let i = 0; i < rawProperties.length; i++) {
